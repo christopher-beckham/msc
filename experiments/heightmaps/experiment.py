34d62ae50a3fc4022e1a45ff247ee3f9aca38304
Christopher Beckham <chrispy645@gmail.com>
2016-03-29 14:09:29 -0400

    Restructuring

--- experiments/heightmaps/experiment.py
+++ experiments/heightmaps/experiment.py
@@ -15,22 +15,12 @@ from time import time
 import train_ae
 import cPickle as pickle
 
-if __name__ == '__main__':
-
-    np.random.seed(0)
-    random.seed(0)
-
+def exp1():
     out_pkl = sys.argv[1]
-
     x_filename = os.environ["DATA_DIR"] + "/train/train_data.npy"
     #x_filename = "train_data_minimal.npy"
     X_train = np.load(x_filename).astype("float32")
-
     sys.stderr.write("X_train shape: %s\n" % str(X_train.shape))
-
-    #X_train_fake = np.random.normal(0, 1, size=(10, 1, 256, 256))
-    #X_train_fake = X_train_fake.astype("float32")
-
     args = dict()
     args["X_train"] = X_train
     #args["rmsprop"] = True
@@ -38,8 +28,33 @@ if __name__ == '__main__':
     args["learning_rate"] = 0.01
     args["batch_size"] = 128
     args["momentum"] = 0.9
-    args["p"] = 0
     args["out_pkl"] = out_pkl
     args["in_pkl"] = "output/model.pkl"
-
+    args["config"] = "basic.py"
     train_ae.train(args)
+
+def exp2():
+    out_pkl = sys.argv[1]
+    x_filename = os.environ["DATA_DIR"] + "/train/train_data.npy"
+    #x_filename = "train_data_minimal.npy"
+    X_train = np.load(x_filename).astype("float32")
+    sys.stderr.write("X_train shape: %s\n" % str(X_train.shape))
+    args = dict()
+    args["X_train"] = X_train
+    #args["rmsprop"] = True
+    args["num_epochs"] = 30
+    args["learning_rate"] = 0.01
+    args["batch_size"] = 128
+    args["momentum"] = 0.9
+    args["out_pkl"] = out_pkl
+    #args["in_pkl"] =
+    args["config"] = "basic2.py"
+    train_ae.train(args)
+
+if __name__ == '__main__':
+
+    np.random.seed(0)
+    random.seed(0)
+
+    #exp1()
+    exp2()
--- experiments/heightmaps/train_ae.py
+++ experiments/heightmaps/train_ae.py
@@ -13,46 +13,12 @@ import random
 from time import time
 import cPickle as pickle
 
-def get_net(args):
-    l_in = InputLayer( (None, 1, 256, 256) )
-    l_noise = GaussianNoiseLayer(l_in, args["sigma"] if "sigma" in args else 0)
-    l_conv1 = Conv2DLayer(
-        l_noise, num_filters=32, filter_size=(3,3), nonlinearity=tanh)
-    l_mp1 = MaxPool2DLayer(l_conv1, pool_size=(2,2))
-    #l_drop1 = DropoutLayer(l_mp1, p=p)
-    l_conv2 = Conv2DLayer(
-        l_mp1, num_filters=64, filter_size=(3,3), nonlinearity=tanh)
-    l_mp2 = MaxPool2DLayer(l_conv2, pool_size=(2,2))
-    #l_drop2 = DropoutLayer(l_mp2, p=p)
-    l_conv3 = Conv2DLayer(
-        l_mp2, num_filters=128, filter_size=(3,3), nonlinearity=tanh)
-    #l_drop3 = DropoutLayer(l_conv3, p=p)
-    l_conv4 = Conv2DLayer(
-        l_conv3, num_filters=128, filter_size=(3,3), nonlinearity=tanh)
-    #l_mp3 = MaxPool2DLayer(l_conv4, pool_size=(3,3))
-    #l_drop4 = DropoutLayer(l_mp3, p=p)
-    #l_conv5 = Conv2DLayer(
-    #    l_mp3, num_filters=256, filter_size=(3,3), nonlinearity=tanh, W=GlorotUniform(gain="relu"))
-    #l_drop5 = DropoutLayer(l_conv5, p=p)
-    #l_conv6 = Conv2DLayer(
-    #    l_conv5, num_filters=256, filter_size=(3,3), nonlinearity=tanh, W=GlorotUniform(gain="relu"))
-    #l_drop6 = DropoutLayer(l_conv6, p=p)
-    l_out = l_conv4
-    for layer in get_all_layers(l_out)[::-1]:
-        if isinstance(layer, InputLayer):
-            break
-        l_out = InverseLayer(l_out, layer)
-    l_out = NonlinearityLayer(l_out, nonlinearity=sigmoid)
-    sys.stderr.write( "number of params: %i\n" % count_params(l_out) )
-    for layer in get_all_layers(l_out):
-        sys.stderr.write("%s,%s\n" % (layer, layer.output_shape))
-    return l_out
-
 def prepare(args):
 
     X = T.tensor4('X')
 
-    l_out = get_net(args)
+    config = imp.load_source("config", args["config"])
+    l_out = config.get_net(args)
     net_out = get_output(l_out, X, deterministic=True)
 
     loss = squared_error(net_out, X).mean()
