da9b45e9d2ba29775c05a88600f9f9c95814a560
Christopher Beckham <chrispy645@gmail.com>
2016-03-30 15:25:58 -0400

    add exp4

--- experiments/heightmaps/experiment.py
+++ experiments/heightmaps/experiment.py
@@ -15,7 +15,7 @@ from time import time
 import train_ae
 import cPickle as pickle
 
-def exp1():
+def exp():
     out_pkl = sys.argv[1]
     x_filename = os.environ["DATA_DIR"] + "/train/train_data.npy"
     #x_filename = "train_data_minimal.npy"
@@ -61,12 +61,29 @@ def exp3():
     args["X_train"] = X_train
     args["num_epochs"] = 30
     args["learning_rate"] = 0.01
-    sys.stderr.write("for exp2 using batch size of 32 instead of 128...\n")
+    sys.stderr.write("for exp2 using batch size of 64 instead of 128...\n")
     args["batch_size"] = 64
     args["momentum"] = 0.9
     args["out_pkl"] = out_pkl
     #args["in_pkl"] =
     args["config"] = "basic_avg_pool.py"
+    train_ae.train(args)
+
+def exp4():
+    out_pkl = sys.argv[1]
+    x_filename = os.environ["DATA_DIR"] + "/train/train_data.npy"
+    X_train = np.load(x_filename).astype("float32")
+    sys.stderr.write("X_train shape: %s\n" % str(X_train.shape))
+    args = dict()
+    args["X_train"] = X_train
+    args["num_epochs"] = 50
+    args["learning_rate"] = 0.01
+    sys.stderr.write("for exp2 using batch size of 64 instead of 128...\n")
+    args["batch_size"] = 64
+    args["momentum"] = 0.9
+    args["out_pkl"] = out_pkl
+    #args["in_pkl"] =
+    args["config"] = "vgg_a_subset.py"
     train_ae.train(args)
 
 if __name__ == '__main__':
@@ -76,4 +93,5 @@ if __name__ == '__main__':
 
     #exp1()
     #exp2()
-    exp3()
+    #exp3()
+    exp4()
