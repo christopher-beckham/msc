b152b0c4e63b6a64aba892a3ce879351eeb317ef
Christopher Beckham <chrispy645@gmail.com>
2016-05-30 10:13:58 -0400

    fix gitignore and add sh

---
+++ experiments/lasagne_additions/experiment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CUDA_LAUNCH_BLOCKING=0 \
+CIFAR10_EXP_2=1 \
+THEANO_FLAGS=mode=FAST_RUN,lib.cnmem=0.95,optimizer_excluding=inplace,device=gpu1,allow_gc=True,floatX=float32,nvcc.fastmath=True,profile=False,dnn.conv.algo_fwd=time_once,dnn.conv.algo_bwd_filter=time_once,dnn.conv.algo_bwd_data=time_once \
+  python -u stochastic_depth_resnet.py
